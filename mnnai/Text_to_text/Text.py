9f9f7466ca8ca3fea8f54dd21b86979b6a5883e1
Mkshustov <135620551+mkshustov@users.noreply.github.com>
2024-10-30 13:51:55 +0400

    Update Text.py

--- mnnai/Text_to_text/Text.py
+++ mnnai/Text_to_text/Text.py
@@ -1,7 +1,7 @@
-from mnnai import ServerError, url
-import json
+from mnnai import url
 import requests
 import aiohttp
+import json
 
 
 async def AsyncText(data):
@@ -20,8 +20,6 @@ async def AsyncText(data):
 
     async with aiohttp.ClientSession() as session:
         async with session.post(f"{url}/v1/chat/complection", headers=headers, json=payload) as response:
-            if response.status == 404:
-                raise ServerError('The server did not respond')
             async for chunk in response.content.iter_chunks():
                 if chunk[0]:
                     yield json.loads(chunk[0].decode('utf-8'))
@@ -42,6 +40,4 @@ def Text(data):
     }
 
     response = requests.post(f"{url}/v1/chat/complection", headers=headers, json=payload)
-    if response.status_code == 404:
-        raise ServerError('The server did not respond')
     return response.json()
