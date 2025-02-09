1a3e794c4839ecf47ca4eaadb85a12aa11d352a8
Mkshustov <135620551+mkshustov@users.noreply.github.com>
2025-02-09 20:53:48 +0400

    Update __init__.py

--- mnnai/__init__.py
+++ mnnai/__init__.py
@@ -1,25 +1,30 @@
+from types import SimpleNamespace
 import requests
 import json
 
 
 url = "https://api.mnnai.ru"
-version = "4.5.2"
-
+version = "5.0.0"
 
 class ServerError(Exception):
     def send_data(self, data):
         raise ServerError("")
 
-
 class VersionNotFoundError(Exception):
     def send_data(self, data):
         raise VersionNotFoundError("")
 
+def change(d):
+    if isinstance(d, dict):
+        return SimpleNamespace(**{k: change(v) for k, v in d.items()})
+    elif isinstance(d, list):
+        return [change(i) for i in d]
+    else:
+        return d
 
 def GetModels():
     response = requests.get(f"{url}/models").json()
     return json.dumps(response, indent=4, ensure_ascii=False)
-
 
 def get_pypi_version():
     try:
