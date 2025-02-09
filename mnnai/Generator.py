2643c74be8cfcdb8b970be9a15f898137877f2a4
Mkshustov <135620551+mkshustov@users.noreply.github.com>
2025-02-09 20:55:10 +0400

    Update Generator.py

--- mnnai/Generator.py
+++ mnnai/Generator.py
@@ -1,6 +1,6 @@
 from mnnai import ServerError, GetModels
+from mnnai import SyncAI, AsyncAI
 from datetime import datetime
-from mnnai import AI
 
 
 def valid(messages):
@@ -23,21 +23,39 @@ def valid(messages):
     return True
 
 
+def check_input(messages, model):
+    if not messages:
+        raise ValueError("The 'prompt' parameter must be filled in.")
+    if not valid(messages):
+        raise ValueError("Incorrect messages")
+    if not model:
+        raise ValueError("The 'model' parameter must be filled in.")
 
 class MNN:
-    def __init__(self, key: str = '', id: str = '', max_retries: int = 0, timeout: float = 600):
-        if not key:
-            raise ValueError("The API key is not filled in. Please provide a valid API key.")
-        if not id:
-            raise ValueError("The 'id' parameter must be filled in.")
-
+    def __init__(self, key: str = '', max_retries: int = 0, timeout: float = 600, debug: bool = False):
         self.key = key
-        self.id = id
         self.max_retries = max_retries
         self.timeout = timeout
+        self.debug = debug
+        self.chat = Chat(key, max_retries, timeout, debug)
+        self.images = Images(key, max_retries, timeout, debug)
 
-    def Image_create(self, prompt: '', model: ''):
+    def GetModels(self):
+        return GetModels()
+
+
+class Images:
+    def __init__(self, key, max_retries, timeout, debug):
+        self.key = key
+        self.max_retries = max_retries
+        self.timeout = timeout
+        self.debug = debug
+
+    def create(self, prompt: '', model: ''):
         start_time = datetime.now()
+        if self.debug:
+            print("Analyzing information entered by the user")
+
         if not prompt:
             raise ValueError("The 'prompt' parameter must be filled in.")
         if not model:
@@ -46,59 +64,140 @@ class MNN:
         data = {
             'prompt': prompt,
             'model': model,
-            'id': self.id,
             'key': self.key,
             'max_retries': self.max_retries,
-            'timeout': self.timeout
+            'timeout': self.timeout,
+            'debug': self.debug
         }
         attempts = 0
         while attempts < self.max_retries + 1:
             if attempts >= 1:
                 print(f"Attempt {attempts+1}")
-            image = AI.Image(data=data)
-            if 'Error' in image:
-                if image['Error'] != 'Sorry, none of the providers responded, please use a different model':
-                    raise ServerError(image['Error'])
+            image = SyncAI.Image(data=data)
+
+            if getattr(image, 'Error', None):
+                if image.Error != 'Sorry, none of the providers responded, please use a different model':
+                    raise ServerError(image.Error)
                 attempts += 1
             else:
                 end_time = datetime.now()
                 time = end_time - start_time
-                image['data'][0]['time']['total time'] = str(time)
+                setattr(image.data[0].time, "total time", str(time))
                 return image
         raise ServerError('Sorry, none of the providers responded, please use a different model')
 
-    def chat_create(self, model: str = '', messages: list = []):
-        if not messages:
+    async def async_create(self, prompt: '', model: ''):
+        start_time = datetime.now()
+        if self.debug:
+            print("Analyzing information entered by the user")
+
+        if not prompt:
             raise ValueError("The 'prompt' parameter must be filled in.")
-        if not valid(messages):
-            raise ValueError("Incorrect messages")
         if not model:
             raise ValueError("The 'model' parameter must be filled in.")
 
         data = {
-            'messages': messages,
+            'prompt': prompt,
             'model': model,
-            'id': self.id,
             'key': self.key,
             'max_retries': self.max_retries,
-            'timeout': self.timeout
+            'timeout': self.timeout,
+            'debug': self.debug
+        }
+        attempts = 0
+        while attempts < self.max_retries + 1:
+            if attempts >= 1:
+                print(f"Attempt {attempts+1}")
+            image = await AsyncAI.Image(data=data)
+
+            if getattr(image, 'Error', None):
+                if image.Error != 'Sorry, none of the providers responded, please use a different model':
+                    raise ServerError(image.Error)
+                attempts += 1
+            else:
+                end_time = datetime.now()
+                time = end_time - start_time
+                setattr(image.data[0].time, "total time", str(time))
+                return image
+        raise ServerError('Sorry, none of the providers responded, please use a different model')
+
+
+class Chat:
+    def __init__(self, key, max_retries, timeout, debug):
+        self.key = key
+        self.max_retries = max_retries
+        self.timeout = timeout
+        self.debug = debug
+
+    def create(self, messages: list, model: str = '', stream: bool = False):
+        if self.debug:
+            print("Analyzing information entered by the user")
+
+        check_input(messages, model)
+
+        data = {
+            'messages': messages,
+            'model': model,
+            'key': self.key,
+            'max_retries': self.max_retries,
+            'timeout': self.timeout,
+            'stream': stream,
+            'debug': self.debug
         }
 
         attempts = 0
         while attempts < self.max_retries + 1:
             if attempts >= 1:
-                print(f"Attempt {attempts + 1}")
-            text = AI.Text(data)
-            if 'Error' in text:
-                if text['Error'] != 'Sorry, none of the providers responded, please use a different model':
-                    raise ServerError(text['Error'])
+                if self.debug:
+                    print(f"Attempt {attempts + 1}")
+
+            text = SyncAI.Text(data)
+
+            if getattr(text, 'Error', None):
+                if text.Error != 'Sorry, none of the providers responded, please use a different model':
+                    raise ServerError(text.Error)
                 attempts += 1
             else:
                 return text
 
-        import time
-        time.sleep(0.5)
         raise ServerError('Sorry, none of the providers responded, please use a different model')
 
-    def GetModels(self):
-        return GetModels()
+    async def async_create(self, messages: list, model: str = '', stream: bool = False):
+        if self.debug:
+            print("Analyzing information entered by the user")
+
+        check_input(messages, model)
+
+        data = {
+            'messages': messages,
+            'model': model,
+            'key': self.key,
+            'max_retries': self.max_retries,
+            'timeout': self.timeout,
+            'stream': stream,
+            'debug': self.debug
+        }
+
+        attempts = 0
+        while attempts < self.max_retries + 1:
+            if attempts >= 1:
+                if self.debug:
+                    print(f"Attempt {attempts + 1}")
+            if stream:
+                async def async_generator():
+                    async for token in AsyncAI.StreamText(data):
+                        yield token
+
+                return async_generator()
+
+            else:
+                text = await AsyncAI.Text(data)
+
+                if getattr(text, 'Error', None):
+                    if text.Error != 'Sorry, none of the providers responded, please use a different model':
+                        raise ServerError(text.Error)
+                    attempts += 1
+                else:
+                    return text
+
+        raise ServerError('Sorry, none of the providers responded, please use a different model')
